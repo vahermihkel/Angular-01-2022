cb455bb3a4c1d8db7f2ed0ca1e36ffcaf92b9e54
mihkelvaher <mihkel.vaher@industry62.com>
2022-01-20 14:07:49 +0200

    Muutujad + ngFor + click

--- veebipood/src/app/avaleht/avaleht.component.ts
+++ veebipood/src/app/avaleht/avaleht.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avaleht.component.css']
 })
 export class AvalehtComponent implements OnInit {
+  kahendV22rtus = true; // boolean
+  kahendV22rtusKaks = false; // boolean
 
-  constructor() { }
+  // string
+  s6nalineMuutuja = "jutumärkide sees mingi sõna või lause või täht";
+
+  // number
+  numbrilineMuutuja = 31312;
+
+  constructor() { 
+    console.log("kui Component ehitatakse valmis siis pannakse esimene constructor käima");
+    console.log("constructori sisse pannakse ühendusi erinevate failidega");
+  }
 
   ngOnInit(): void {
+    console.log("AvalehtComponent käimaminemise funktsioon -  see läheb");
+    console.log("käima siis, kui kasutaja läheb siia componendi peale ja");
+    console.log("HTML käivitub, aga ngOnInit funktsioon läheb käima vahetult enne HTMLi");
+  }
+
+  onLisaOstukorvi() {
+    console.log("ostukorvi lisamise funktsionaalsus töötab!");
+    this.kahendV22rtus = !this.kahendV22rtus;
+    this.kahendV22rtusKaks = !this.kahendV22rtusKaks;
+    // this.numbrilineMuutuja = !this.numbrilineMuutuja;
   }
 
 }
--- veebipood/src/app/ostukorv/ostukorv.component.ts
+++ veebipood/src/app/ostukorv/ostukorv.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ostukorv.component.css']
 })
 export class OstukorvComponent implements OnInit {
+  toodeNimi = "Coca coca";
+  toodeHind = 1;
+  toodeAktiivne = false;
 
-  constructor() { }
+  toodeNimi2 = "Fanta";
+  toodeHind2 = 1.5;
+  toodeAktiivne2 = true;
+
+  // JSON kuju
+
+  constructor() { 
+    console.log("constructor läheb käima");
+  }
 
   ngOnInit(): void {
+    console.log("mindi OstukorvComponent lehele");
+  }
+
+  onEemaldaOstukorvist() {
+    console.log("eemaldatud ostukorvist");
   }
 
 }
